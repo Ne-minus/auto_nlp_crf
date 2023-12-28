b9a5afd812804bcc1a56c5d7adb1ace0cc72baf0
Виктория Князькова <vknyazkova01@gmail.com>
2023-12-29 02:02:39 +0300

    bert pipeline

--- bert_sentiment.py
+++ bert_sentiment.py
@@ -1,61 +1,117 @@
-class SentimentBert:
+from typing import Dict
 
-  def __init__(self, model_name, prompt_type, lang):
-    self.model = model_name
 
-    if lang == 'ru':
-      self.rubert_map = {'Whole':'впечатление вцелом','Service':'сервис',
-                         'Food':'еда', 'Interior': 'интерьер',
-                         'Price': 'цена'}
-      self.acc_map = {'NEGATIVE': 'negative', 
-                      'NEUTRAL': 'neutral', 
-                      'POSITIVE': 'positive'}
-    elif lang == 'en':
-      self.acc_map = {'1 star': 'negative', 
-                      '2 stars': 'negative', 
-                      '3 stars': 'neutral', 
-                      '4 stars': 'positive', 
-                      '5 stars': 'positive'}
-    self.prompt_type = prompt_type
-  
-  def prompt_types(self, prompt_type, sent, text=None, aspect=None, category=None):
-    if prompt_type == 1:
-      prompt = f'{sent} [SEP] {aspect}, {category}'
+import pandas as pd
+from tqdm import tqdm
+from transformers import pipeline
 
-    elif prompt_type == 2:
-      prompt = f"Определи тональность аспекта '{aspect}' в следующем тексте: '{sent}'"
+from abstract_pipelines import ABSAComponent, ABSADataset
 
-    elif prompt_type == 3:
-      prompt = sent
 
-    # для тональности всей категории в отзыве
-    elif prompt_type == 4:
-      prompt = f'{text} [SEP] {category}'
+rubert_sent_map = {'NEGATIVE': 'negative',
+                   'NEUTRAL': 'neutral',
+                   'POSITIVE': 'positive'}
 
-    return prompt
+multi_sent_map = {'1 star': 'negative',
+                  '2 stars': 'negative',
+                  '3 stars': 'neutral',
+                  '4 stars': 'positive',
+                  '5 stars': 'positive'}
 
-  def predict(self, file):
-    df = pd.read_csv(file, sep='\t')
-    sentiment_analysis = pipeline('text-classification', model=self.model)
-    data = []
 
-    for _, row in df.iterrows():
-      if self.prompt_type == 1:
-        result = sentiment_analysis(prompt_types(self.prompt_type, row.sentence, row.aspect, row.category))
-      elif self.prompt_type == 2:
-        result = sentiment_analysis(prompt_types(self.prompt_type, row.sentence, row.aspect))
-      elif self.prompt_type == 3:
-        result = sentiment_analysis(prompt_types(self.prompt_type, row.sentence))
-      elif self.prompt_type == 4:
-        result = sentiment_analysis(prompt_types(self.prompt_type, row.text, row.category))
+class ATSBert(ABSAComponent):
 
-      for sentiment in result:
-        label = sentiment['label']
-        score = sentiment['score']
+    oblig_attrs = {
+        'train': {'reviews', 'aspects'},
+        'inference': {'reviews', 'parsed_aspects_'}
+    }
 
-      data.append([label])
-    labels = pd.DataFrame(data, columns=['sentiment'])
+    cats_transl = {
+        'Whole': {'ru': 'впечатление в целом', 'en': 'Whole'},
+        'Service': {'ru': 'сервис', 'en': 'Service'},
+        'Food': {'ru': 'еда', 'en': 'Food'},
+        'Interior': {'ru': 'интерьер', 'en': 'Interior'},
+        'Price': {'ru': 'цена', 'en': 'Price'}
+    }
 
-    pred_file = pd.concat([df, labels.sentiment.map(self.acc_map)], axis=1).to_csv('aspect_sentiment.csv')
-    
-    return pred_file
+    def __init__(self,
+                 model_name: str,
+                 sentiment_map: Dict[str, str],
+                 prompt_type: int = 1,
+                 lang: str = 'ru'
+                 ):
+        self.model_name = model_name
+        self.sentiment_map = sentiment_map
+        self.prompt_type = prompt_type
+        self.lang = lang
+        self.load_model()
+
+    def load_model(self):
+        sentiment_analysis = pipeline('text-classification', model=self.model_name)
+        self.model = sentiment_analysis
+
+    def fit(self,
+            train_dataset: ABSADataset):
+        ...
+
+    @staticmethod
+    def from_pretrained(model_name: str,
+                        sentiment_map: Dict[str, str],
+                        prompt_type: int = 1,
+                        lang: str = 'ru'):
+        model = ATSBert(model_name, sentiment_map, prompt_type, lang)
+        return model
+
+    @staticmethod
+    def fill_prompt(prompt_type, sent, text=None, aspect=None, category=None):
+        if prompt_type == 1:
+            prompt = f'{sent} [SEP] {aspect}, {category}'
+        elif prompt_type == 2:
+            prompt = f"Определи тональность аспекта '{aspect}' в следующем тексте: '{sent}'"
+        elif prompt_type == 3:
+            prompt = sent
+        return prompt
+
+    def translate_category(self,
+                           data: pd.DataFrame):
+        data['category'] = data.apply(lambda x: self.cats_transl[x['category']][self.lang], axis=1)
+        return data
+
+    def _predict(self,
+                 ats_input: pd.DataFrame):
+        data = []
+        print('Predicting sentiments...')
+        for _, row in tqdm(ats_input.iterrows(), total=ats_input.shape[0]):
+            result = self.model(self.fill_prompt(self.prompt_type, row['sent'], row['aspect'], row['category']))
+            data.append(self.sentiment_map[result[0]['label']])
+        ats_input['sentiment'] = data
+        return ats_input
+
+    def _validate_dataset(self,
+                         dataset: ABSADataset,
+                         part: str):
+        missing = self.oblig_attrs[part] - set(dir(dataset))
+        if missing:
+            raise ValueError(f'Dataset missing the following obligatory attrs: {missing}')
+
+    def predict(self,
+                test_dataset: ABSADataset):
+        self._validate_dataset(test_dataset, part='inference')
+        ats_input = test_dataset.ats_input()
+        ats_input['category'] = self.translate_category(ats_input)['category']
+        return self._predict(ats_input)
+
+
+if __name__ == '__main__':
+    import yaml
+
+    with open('configs.yml', 'r') as file:
+        config = yaml.safe_load(file)
+
+    bert_config = config['components']['bert_ats']
+    checkpoints = config['checkpoints']
+    ats_model = globals()[bert_config['class_name']].from_pretrained(**checkpoints['bert_ats'])
+
+    train_dataset = ABSADataset(config['dataset'], 'dev')
+
+    print(ats_model.predict(train_dataset))
